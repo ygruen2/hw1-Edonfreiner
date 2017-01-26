2295e28fd506117c2c32302ef1be3ad5e9ef02bf
edonfreiner <edonfreiner@10.150.44.252>
2017-01-25 21:35:29 -0500

    Finished Homework1

--- src/main/java/mco232/Utilities.java
+++ src/main/java/mco232/Utilities.java
@@ -2,9 +2,27 @@ package mco232;
 
 public class Utilities {
     
-    //I have arrived
-        public static boolean testCode()
-    {
-        return true; 
-    }
+public static double fahrenheitToCelsius(double temp){
+    return (temp - 32) * (5/9);
+    
+}
+public static double celsiusToFahrenheit(double temp){
+    return (temp * (9/5)) + 32;
+}
+public static double kelvinToCelsius(double temp){
+    return temp - 273.15;
+}
+public static double celsiusToKelvin(double temp){
+        return temp + 273.15;
+
+}
+public static double fahrenheitToKelvin(double temp){
+    return ((temp - 32) * (5 / 9)) + 273.15;
+}
+public static double kelvinToFahrenheit(double temp){
+        return (temp * (9/5)) + 32 - 273.15;
+
+}
+    
+   
 }
--- src/test/java/mco232/TestUtilities.java
+++ src/test/java/mco232/TestUtilities.java
@@ -1,14 +1,39 @@
 package mco232;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 public class TestUtilities {
     
     @Test
-    public void test1(){
-        boolean actual = Utilities.testCode();
-        assertTrue(actual);       
+    public void fahrenheitToCelsius(){
+        Assert.assertEquals(0, Utilities.fahrenheitToCelsius(32),99);
+    } 
+    @Test
+    public void celsiusToFahrenheit(){
+        Assert.assertEquals(32, Utilities.celsiusToFahrenheit(0),2);
     }
+     @Test
+    public void kelvinToCelsius(){
+        Assert.assertEquals(-273.15, Utilities.kelvinToCelsius(0),2);
+    }
+     
+     @Test
+    public void celsiusToKelvin(){
+        Assert.assertEquals(273.15, Utilities.celsiusToKelvin(0),2);
+    }
+     
+     @Test
+    public void fahrenheitToKelvin(){
+        Assert.assertEquals(273.15, Utilities.fahrenheitToKelvin(32),2);
+    }
+    @Test
+    public void kelvinToFahrenheit(){
+        Assert.assertEquals(32, Utilities.kelvinToFahrenheit(273.15),2);
+    }
+     
+     
+    
     
 }
